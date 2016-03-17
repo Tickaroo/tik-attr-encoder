8fc0f055afe4f7ebf5fa9855aa81f88445c59638
Anton Pawlik <anton.pawlik@gmail.com>
2016-03-17 15:01:56 +0100

    more tests

--- test/to-attrs.js
+++ test/to-attrs.js
@@ -1,11 +1,12 @@
 var expect = require('chai').expect;
 var toAttrs = require('../to-attrs.js');
+var toHTML = require('../to-html.js');
 var testData = require('./testdata.js');
 var jsdom = require('jsdom');
 
 describe('toAttrs', function () {
   var doc = jsdom.jsdom();
-  testData.forEach(function (testCase) {
+  var arrayIt = function (testCase) {
     it(testCase.test, function () {
       var div = doc.createElement('div');
       div.innerHTML = testCase.html;
@@ -13,5 +14,32 @@ describe('toAttrs', function () {
         proxyDocument: doc
       })).to.deep.equal(testCase.attrs);
     });
+  };
+  testData.forEach(arrayIt);
+
+  [{
+    test: 'unknown <tags>',
+    html: 'asdfasdf<ul>asdf</ul>asdfr',
+    title: 'asdfasdfasdfasdfasdfr',
+    attrs: []
+  }, {
+    test: 'removes <br>',
+    html: 'asdfasdf\n<br>asdf<u>asdf</u>asdfr',
+    title: 'asdfasdf\nasdfasdfasdfr',
+    attrs: [{
+      _type: 'Tik::ApiModel::Text::UnderlineSpan',
+      start: 13,
+      end: 16
+    }]
+  }].forEach(arrayIt);
+
+  it('to hell and back and back again', function () {
+    var div = doc.createElement('div');
+    div.innerHTML = 'asdfasdf<br>\n\n<br>asdf<u>asdf</u>asdfr';
+    a = toAttrs(div, {
+      proxyDocument: doc
+    });
+    expect(toHTML(div.textContent, a)).to.equal('asdfasdf\n\nasdf<u>asdf</u>asdfr');
   });
+
 });
