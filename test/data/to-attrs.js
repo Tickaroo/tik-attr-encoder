69e05b86c60e079fd7a23f7e636fb258a3d6fcf8
Anton Pawlik <anton.pawlik@gmail.com>
2016-03-25 10:43:28 +0100

    handle unclosed tags and attrs

--- test/data/to-attrs.js
+++ test/data/to-attrs.js
@@ -1,1 +1,10 @@
-module.exports = [];
+module.exports = [{
+  test: 'unclosed tag',
+  html: 'asdfasdfasdfa<strong>sdfasdfr',
+  title: 'asdfasdfasdfasdfasdfr',
+  attrs: [{
+    _type: 'Tik::ApiModel::Text::BoldSpan',
+    start: 13,
+    end: 21
+  }]
+}];
--- test/data/to-html.js
+++ test/data/to-html.js
@@ -16,4 +16,22 @@ module.exports = [{
     start: 5,
     end: 3
   }]
+}, {
+  test: 'unclosed tag',
+  html: 'asdfasdfasdfa<strong>sdfasdfr</strong>',
+  title: 'asdfasdfasdfasdfasdfr',
+  attrs: [{
+    _type: 'Tik::ApiModel::Text::BoldSpan',
+    start: 13,
+    end: 44
+  }]
+}, {
+  test: 'unclosed tag at the very end',
+  html: 'asdfasdfasdfasdfasdfr<strong></strong>',
+  title: 'asdfasdfasdfasdfasdfr',
+  attrs: [{
+    _type: 'Tik::ApiModel::Text::BoldSpan',
+    start: 21,
+    end: 44
+  }]
 }];
